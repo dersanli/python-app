07130d7f12b0b33d72fb2bbf19003c4b8c6752b8
güneş <ersanligunes@gmail.com>
2024-07-01 14:33:29 +0000

    seperation

--- import csv.py
+++ import csv.py
@@ -1,8 +1,25 @@
 import csv
 
-l = 'data-juBjDEKS03Bds6wun-5kM.csv'
-with open(l,'r') as file:
+data_file = 'data-juBjDEKS03Bds6wun-5kM.csv'
+
+
+with open(data_file,'r') as file:
     csvreader = csv.reader(file)
 
     for row in csvreader:
-        print(row)
+        country = row[1]
+        region = row[2]
+        secret = row[3]
+        company = row[4]
+        region1 = row[5]
+        currency = row[6]
+        fullname = row[0]
+        names = fullname.split(' ')
+      
+        if (len(names)==2):
+            surname = names[1]
+            print(surname, secret, currency)
+        else:
+            print(secret, currency)
+     
+        
