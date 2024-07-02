4bfff439d81b25078904a3b11547c0f5e8d2c51b
ulyana <fischergculyana@gmail.com>
2024-07-02 10:40:00 +0000

    read data

--- import csv.py
+++ import csv.py
@@ -1,19 +1,25 @@
 import csv
 
-data_file = 'data-juBjDEKS03Bds6wun-5kM.csv'
+data_files = ['data-juBjDEKS03Bds6wun-5kM.csv','data-mhC2yxsAXZTSCXLFsrPLU.csv','data-oBewWktMX5Z6JGaB-2V3z.csv','data-sm3_cxHiLqpa38sOpi7hk.csv', 'data-yzRZxpoxRgFCQr55Cc6kl.csv']
 
-with open(data_file,'r') as file:
-    csvreader = csv.reader(file)
-    
-    for row in csvreader:
-        country = row [1]
-        currency = row [6]
-        fullname = row [0]
-        names = fullname.split(' ')
 
-        
-        if (len(names)==2):
-            surname = names[1]
-            print(surname, country, currency)
-        else:
-            print(country,currency)
+
+def read_csv(file_name):
+    print('============>',file_name)
+    with open(file_name, 'r') as file:
+        csvreader = csv.reader(file)
+
+        for row in csvreader:
+            print(row)
+
+
+for data_file in data_files:
+    read_csv(data_file)
+  
+
+
+
+
+# with fileinput.input files ='data_file1', 'data_file2', 'data_file3', 'data_file4', 'data_file5') as f:
+#     for line in f:
+#         print(line)
