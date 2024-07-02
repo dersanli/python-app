7c5b6bf35d257071f0a7b244c45f3e4f03348f07
güneş <ersanligunes@gmail.com>
2024-07-02 11:00:49 +0000

    Pending changes exported from your codespace

--- import csv.py
+++ import csv.py
@@ -1,25 +1,14 @@
 import csv
 
-data_file = 'data-juBjDEKS03Bds6wun-5kM.csv'
+data_files = ['data-juBjDEKS03Bds6wun-5kM.csv', 'data-mhC2yxsAXZTSCXLFsrPLU.csv', 'data-oBewWktMX5Z6JGaB-2V3z.csv', 'data-sm3_cxHiLqpa38sOpi7hk.csv','data-yzRZxpoxRgFCQr55Cc6kl.csv']
 
+def read_csv(file_name):
+    print('============>', file_name)
+    with open(file_name, 'r') as file:
+        csvreader = csv.reader(file)
 
-with open(data_file,'r') as file:
-    csvreader = csv.reader(file)
+        for row in csvreader:
+            print(row)
 
-    for row in csvreader:
-        country = row[1]
-        region = row[2]
-        secret = row[3]
-        company = row[4]
-        region1 = row[5]
-        currency = row[6]
-        fullname = row[0]
-        names = fullname.split(' ')
-      
-        if (len(names)==2):
-            surname = names[1]
-            print(surname, secret, currency)
-        else:
-            print(secret, currency)
-     
-        
+for data_file in data_files:
+    read_csv(data_file)
