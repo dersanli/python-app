12b220de737b54647734bd636b425ff2c1330fef
güneş <ersanligunes@gmail.com>
2024-07-02 13:49:08 +0000

    file manipulation

--- import csv.py
+++ import csv.py
@@ -1,6 +1,7 @@
 import csv
 
 data_files = ['data-juBjDEKS03Bds6wun-5kM.csv', 'data-mhC2yxsAXZTSCXLFsrPLU.csv', 'data-oBewWktMX5Z6JGaB-2V3z.csv', 'data-sm3_cxHiLqpa38sOpi7hk.csv','data-yzRZxpoxRgFCQr55Cc6kl.csv']
+countries = [] 
 
 def read_csv(file_name):
     print('============>', file_name)
@@ -8,7 +9,30 @@ def read_csv(file_name):
         csvreader = csv.reader(file)
 
         for row in csvreader:
-            print(row)
+            country = row[1]
+            region = row[2]
+            secret = row[3]
+            company = row[4]
+            region1 = row[5]
+            currency = row[6]
+            fullname = row[0]
+            # print(country)
+            if not country in countries:
+                countries.append(country)
+
+def write_csv(file_name):
+    print("I'll write file named: ", file_name)
+    # writing to csv file
+    with open(file_name, 'w') as csvfile:
+        # creating a csv dict writer object
+        writer = csv.DictWriter(csvfile)
+
 
 for data_file in data_files:
     read_csv(data_file)
+
+for country in countries:
+    write_csv(country)
+    
+# print(countries)
+
