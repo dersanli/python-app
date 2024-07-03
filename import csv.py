886b5ff2043b64ba3c754e7916f070244627167e
ulyana <fischergculyana@gmail.com>
2024-07-03 12:48:14 +0000

    data files

--- import csv.py
+++ import csv.py
@@ -1,6 +1,11 @@
 import csv
+import os
+
 data_files = ['data-juBjDEKS03Bds6wun-5kM.csv', 'data-mhC2yxsAXZTSCXLFsrPLU.csv', 'data-oBewWktMX5Z6JGaB-2V3z.csv', 'data-sm3_cxHiLqpa38sOpi7hk.csv','data-yzRZxpoxRgFCQr55Cc6kl.csv']
-countries = [] 
+countries = []
+
+people = []
+
 def read_csv(file_name):
     print('============>', file_name)
     with open(file_name, 'r') as file:
@@ -16,6 +21,7 @@ def read_csv(file_name):
             # print(country)
             if not country in countries:
                 countries.append(country)
+
 fields = ['country', 'region','secret', 'company', 'region1', 'currency', 'fullname']
 
 def write_csv(file_name):
@@ -24,7 +30,10 @@ def write_csv(file_name):
     with open(file_name, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
+
 for data_file in data_files:
     read_csv(data_file)
+
+
 for country in countries:
-    write_csv(country)
+    write_csv( os.path.join( 'data', country))
